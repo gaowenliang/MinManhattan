5c04a2b1d1dd933d9430530843e6834c850c4fb0
gaowenliang <gaowenliang100@gmail.com>
2018-02-12 14:51:34 +0800

    test doww

--- src/test.cpp
+++ src/test.cpp
@@ -7,9 +7,14 @@ main( int argc, char** argv )
 {
     std::vector< Eigen::Vector2d > pts;
     pts.push_back( Eigen::Vector2d( 0, 0 ) );
+    pts.push_back( Eigen::Vector2d( 0, 5 ) );
+    pts.push_back( Eigen::Vector2d( 0, 10 ) );
+    pts.push_back( Eigen::Vector2d( 0, 15 ) );
     pts.push_back( Eigen::Vector2d( 0, 20 ) );
+    pts.push_back( Eigen::Vector2d( 10, 0 ) );
     pts.push_back( Eigen::Vector2d( 20, 0 ) );
     pts.push_back( Eigen::Vector2d( 10, 10 ) );
+    pts.push_back( Eigen::Vector2d( 150, 0 ) );
 
     MinManhattan man( pts );
     std::cout << "Final point is: " << man.getFinalPoint( ).transpose( ) << std::endl;
